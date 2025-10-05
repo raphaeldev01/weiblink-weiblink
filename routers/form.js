876012deb239c683f1be873bcbffdc7f719da18f
Raphael Developer <raphael.developer@hotmail.com>
2025-10-04 23:06:59 -0300

    upload2

--- routers/form.js
+++ routers/form.js
@@ -5,11 +5,22 @@ const telegram = require("../connections/Telegram")
 
 Router.post("/newProject", (req, res) => {
     
+    console.log("aqui1")
+
     if(!req.body) return
+
+    console.log("aqui2")
+
 
     const { name, email, plan, type, tell } = req.body;
 
+    console.log("aqui3")
+
+
     if (!name || !email || !plan || !type || !tell) return res.send(400);
+
+    console.log("aqui4")
+
 
     telegram.SendNewProject({
         email,
